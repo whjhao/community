69e881a3701df4d5877089b62b9d1c41767b8377
whj <944694930@qq.com>
2019-10-30 20:01:12 +0800

    diycii

---
+++ src/test/java/com/whjsprbootcummnity/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whjsprbootcummnity.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
